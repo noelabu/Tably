3c1d7678686e3f7a75e96de1208f9848b8d1838d
Noela Bunag <njsbunag@gmail.com>
2025-07-12 23:31:58 +0800

    Add api endpoints for menu items

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
-from app.api.endpoints import auth
+from app.api.endpoints import auth, menu_items
 from fastapi import APIRouter
 
 api_router = APIRouter()
 
 # Import and include routers from endpoints
-from app.api.endpoints import auth
+from app.api.endpoints import auth, menu_items
 
-# Include the linecoach router with a prefix
+# Include routers with prefixes
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(menu_items.router, prefix="/menu-items", tags=["menu-items"])
